6011e22103e05e74e827829fc706df5286f9de3f
RaviChaudhari <ravi.chaudhari@apaarinfosystems.com>
2022-07-04 15:07:23 +0530

    Add counts

---
+++ GitHubUploadProject/CountClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubUploadProject
+{
+    internal class CountClass
+    {
+        public static int Counts = 0;
+    }
+}
--- GitHubUploadProject/Form1.cs
+++ GitHubUploadProject/Form1.cs
@@ -2,14 +2,17 @@ namespace GitHubUploadProject
 {
     public partial class Form1 : Form
     {
+        CountClass cc = new CountClass();
         public Form1()
         {
             InitializeComponent();
+            CountClass.Counts = 0;
         }
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("You click button");
+            CountClass.Counts = CountClass.Counts+1;
+            MessageBox.Show("You click button "+ CountClass.Counts +" times");
         }
     }
 }
